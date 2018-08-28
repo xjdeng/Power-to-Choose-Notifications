a79b181aa7f0a5adc1d61e80b176d3c186f54013
xjdeng <xjd001@gmail.com>
2018-08-27 19:19:08 -0500

    Use fake_useragent

--- simple.py
+++ simple.py
@@ -1,8 +1,19 @@
 from selenium import webdriver
 import time
+import os
+import pandas as pd
+from fake_useragent import UserAgent
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
+def get_dir():
+    #http://www.karoltomala.com/blog/?p=622
+    return os.path.dirname(os.path.abspath(__file__))
 
 def run(zipcode):
-    browser = webdriver.PhantomJS()
+    ua = UserAgent()
+    caps = DesiredCapabilities.PHANTOMJS
+    caps["phantomjs.page.settings.userAgent"] = ua.random
+    browser = webdriver.PhantomJS(desired_capabilities=caps)
     browser.get("http://www.powertochoose.com")
     zipfield = browser.find_element_by_id('homezipcode')
     zipfield.click()
@@ -11,5 +22,5 @@ def run(zipcode):
     view_results.click()
     time.sleep(1)
     price_eles = browser.find_elements_by_class_name("price")
-    prices = [float(p.text[1:-1]) for p in price_eles]
-    return prices
+    prices = [float(p.text[0:-1]) for p in price_eles]
+    return prices, price_eles
